288359b8ee20c9423a05159e190ac138b70d5421
bongdze <bongdze237@gmail.com>
2023-12-28 23:29:04 +0100

    second supervision

---
+++ lib_with_homepage/lib/screens/sign_in_form.dart
@@ -0,0 +1,104 @@
+import 'package:example/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() {
+    return _SignInScreenState();
+  }
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    openSignup(BuildContext context) {
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (ctx) => const HomeScreen()),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+            'Create an account',
+            style: TextStyle(color: Colors.green),
+          ),
+          iconTheme: const IconThemeData(color: Colors.green),
+          backgroundColor: const Color.fromARGB(160, 230, 230, 250)),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Full Names'),
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Phone Number'),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Password'),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        padding:
+                            const MaterialStatePropertyAll(EdgeInsets.all(16)),
+                        backgroundColor: const MaterialStatePropertyAll(
+                            Color.fromARGB(255, 13, 103, 15)),
+                      ),
+                      onPressed: () {
+                        openSignup(context);
+                      },
+                      child: const Text(
+                        'SIGN UP',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 350),
+              Row(
+                children: [
+                  const Text(
+                    'Already have an account?',
+                    style: TextStyle(color: Colors.green, fontSize: 16),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Log in',
+                      style: TextStyle(color: Colors.green, fontSize: 16),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
